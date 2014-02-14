9097c954bca9027cd7d68c8637d4c2f146488801
luowei <luowei@localhost.steedos.com>
2014-02-14 10:16:33 +0800

    fourth

---
+++ stop.sh
@@ -0,0 +1,9 @@
+thin stop -c /var/steedos/server-push -p 2300 -d
+thin stop -c /var/steedos/server-setup -p 2300 -d
+thin stop -c /var/steedos/server-keyvalue -p 2600 -d
+thin stop -c /var/steedos/server-space -p 3000 -d
+thin stop -c /var/steedos/server-uuflow -p 4000 -d
+thin stop -c /var/steedos/server-attachments -p 5000 -d
+thin stop -c /var/steedos/server-forms -p 6000 -d
+thin stop -c /var/steedos/server-chats -p 7000 -d
+service nginx stop
