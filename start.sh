e62a8f9825d573205e8c34cfc5b85c9c63c27077
luowei <luowei@hotoa.com>
2014-02-18 14:31:25 +0800

    sixth

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-thin start -c /var/steedos/server-push -p 2000 -d
-thin start -c /var/steedos/server-setup -p 2300 -d
-thin start -c /var/steedos/server-keyvalue -p 2600 -d
-thin start -c /var/steedos/server-space -p 3000 -d
-thin start -c /var/steedos/server-uuflow -p 4000 -d
-thin start -c /var/steedos/server-attachments -p 5000 -d
-thin start -c /var/steedos/server-forms -p 6000 -d
-thin start -c /var/steedos/server-chats -p 7000 -d
-thin start -c /var/steedos/server-tasks -p 2700 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-push -p 2000 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-setup -p 2300 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-keyvalue -p 2600 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-space -p 3000 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-uuflow -p 4000 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-attachments -p 5000 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-forms -p 6000 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-chats -p 7000 -d
+thin start -c /var/steedos/steedos-temp/steedos/server-tasks -p 2700 -d
 service nginx start
